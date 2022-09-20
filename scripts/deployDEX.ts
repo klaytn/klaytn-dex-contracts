761f507b71b0933406a602e60eb3f4829b99272d
Sergey <travoltaj237@gmail.com>
2022-09-20 14:41:36 +0300

    [Update] Deployment script modified to save subgraph JSON type data

--- scripts/deployDEX.ts
+++ scripts/deployDEX.ts
@@ -3,12 +3,22 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from 'hardhat';
+import hre, { ethers } from 'hardhat';
+import { BigNumber, Contract } from 'ethers';
+import fs from 'fs';
 import * as dotenv from 'dotenv';
 
+interface DeployDetails {
+  [key: string]: {
+    [key: string]: {
+      address: string;
+      startBlock: number;
+    } }
+}
 let adminsList: string[];
 let sigRequired: string;
-let rewardPerBlock: string;
+let rewardPerBlock: BigNumber;
+let config: DeployDetails;
 dotenv.config();
 
 if (!process.env.ADMIN_LIST && !process.env.SIG_REQUIRED) {
@@ -21,8 +31,29 @@ if (!process.env.ADMIN_LIST && !process.env.SIG_REQUIRED) {
 if (!process.env.REWARD_PER_BLOCK) {
   throw new Error('Please set your rewardPerBlock param (in wei) for farming contract in the .env file');
 } else {
-  rewardPerBlock = JSON.parse(process.env.REWARD_PER_BLOCK as string);
+  rewardPerBlock = BigNumber.from(process.env.REWARD_PER_BLOCK as string);
 }
+
+const writeDeploymentForSubgraph = async (
+  type: string,
+  contractInstance: Contract,
+): Promise<void> => {
+  const { provider, name } = hre.network;
+  const txReceipt = await provider.send('eth_getTransactionReceipt', [contractInstance.deployTransaction.hash]);
+  config = {
+    [name]:
+    {
+      [type]: {
+        address: contractInstance.address,
+        startBlock: BigNumber.from(txReceipt.blockNumber).toNumber(),
+      },
+    },
+  };
+  if (!fs.existsSync('./deployments')) {
+    fs.mkdirSync('./deployments');
+  }
+  fs.writeFileSync(`./deployments/${type}-${name}.json`, JSON.stringify(config, null, 2));
+};
 
 async function main() {
   const accounts = await ethers.getSigners();
@@ -33,15 +64,14 @@ async function main() {
   const wklay = await ethers.getContractFactory('WETH9');
   const wklayInstance = await wklay.deploy();
   await wklayInstance.deployed();
-
-  console.log(`WKLAY deployed to : ${wklayInstance.address}`);
+  console.log(`WKLAY deployed to: ${wklayInstance.address}`);
 
   // Deploy Dex Factory
   const factory = await ethers.getContractFactory('DexFactory');
   const factoryInstance = await factory.deploy(deployer);
   await factoryInstance.deployed();
-
-  console.log(`Dex Factory deployed to : ${factoryInstance.address}`);
+  console.log(`Dex Factory deployed to: ${factoryInstance.address}`);
+  await writeDeploymentForSubgraph('Factory', factoryInstance);
 
   // Deploy Dex Router passing Factory Address and WKLAY Address
   const router = await ethers.getContractFactory('DexRouter');
@@ -50,29 +80,25 @@ async function main() {
     wklayInstance.address,
   );
   await routerInstance.deployed();
-
-  console.log(`Dex Router deployed to :  ${routerInstance.address}`);
+  console.log(`Dex Router deployed to:  ${routerInstance.address}`);
 
   // Deploy Multisig passing adminsList addresses and sigRequired params from .env file
   const multisig = await ethers.getContractFactory('MultiSigWallet');
   const multisigInstance = await multisig.deploy(adminsList, sigRequired);
   await multisigInstance.deployed();
-
-  console.log(`Multisig deployed to : ${multisigInstance.address}`);
+  console.log(`Multisig deployed to: ${multisigInstance.address}`);
 
   // Deploy Multicall
   const multicall = await ethers.getContractFactory('Multicall');
   const multicallInstance = await multicall.deploy();
   await multicallInstance.deployed();
-
-  console.log(`MultiCall deployed to : ${multicallInstance.address}`);
+  console.log(`MultiCall deployed to: ${multicallInstance.address}`);
 
   // Deploy Dex Token
   const dexToken = await ethers.getContractFactory('PlatformToken');
   const dexTokenInstance = await dexToken.deploy('Klaytn DEX', 'KDEX', multisigInstance.address);
   await dexTokenInstance.deployed();
-
-  console.log(`Dex Token deployed to : ${dexTokenInstance.address}`);
+  console.log(`Dex Token deployed to: ${dexTokenInstance.address}`);
 
   // Deploy Farming
   const farming = await ethers.getContractFactory('Farming');
@@ -84,14 +110,15 @@ async function main() {
     multisigInstance.address,
   );
   await farmingInstance.deployed();
-  console.log(`Farming deployed to : ${farmingInstance.address}`);
+  console.log(`Farming deployed to: ${farmingInstance.address}`);
+  await writeDeploymentForSubgraph('Farming', farmingInstance);
 
   // Deploy Staking Factory passing multisig contract's address as a parameter
   const stakingFactory = await ethers.getContractFactory('StakingFactory');
   const stakingFactoryInstance = await stakingFactory.deploy(multisigInstance.address);
   await stakingFactoryInstance.deployed();
-
-  console.log(`Staking Factory deployed to : ${stakingFactoryInstance.address}`);
+  console.log(`Staking Factory deployed to: ${stakingFactoryInstance.address}`);
+  await writeDeploymentForSubgraph('StakingFactory', stakingFactoryInstance);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
